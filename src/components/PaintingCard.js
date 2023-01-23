f5dabbf285c5717032c8bf39e4e5421c0e6d54a8
samanthamarberger <slatham04@gmail.com>
2023-01-23 13:39:35 -0700

    Added painting card function

---
+++ src/components/PaintingCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PaintingCard() {
+    return(
+        <div>
+            <h3>This is where the painting will go</h3>
+        </div>
+    );
+}
+
+export default PaintingCard;
