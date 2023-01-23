d681089578c437bc8cda46598c04b90b63dcd851
samanthamarberger <slatham04@gmail.com>
2023-01-23 12:18:47 -0700

    Added functionality to component (just says NavBar)

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+    return (
+        <div>
+            <h1>NavBar</h1>
+        </div>
+    );
+}
+
+export default NavBar;
