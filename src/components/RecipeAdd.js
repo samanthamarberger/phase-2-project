f5eeb802127b45f9159557fcdb760e0c2b938208
samanthamarberger <slatham04@gmail.com>
2023-01-23 12:37:53 -0700

    Added RecipeAdd function

---
+++ src/components/RecipeAdd.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RecipeAdd() {
+    return (
+        <div>
+            <h1>This is where you add new recipes!</h1>
+        </div>
+    )
+}
+
+export default RecipeAdd;
