428be75958165ec9bc8441f814a92a9e6651ccaf
samanthamarberger <slatham04@gmail.com>
2023-01-25 14:35:41 -0700

    added fetch POST feature to form submit component

--- src/components/PaintingAdd.js
+++ src/components/PaintingAdd.js
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PaintingAdd() {
-    console.log("Addd painting");
+    const [paintingTitle, setPaintingTitle] = useState("");
+    const [paintingImage, setPaintingImage] = useState("");
+    const [paintingArtist, setPaintingArtist] = useState("");
+
+    function handleTitleAddition(e) {
+        setPaintingTitle(e.target.value);
+    }
+    function handleImageAddition(e) {
+        setPaintingImage(e.target.value);
+    }
+    function handleArtistAddition(e) {
+        setPaintingArtist(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const newPainting = {
+            title: paintingTitle,
+            imageSrc: paintingImage,
+            artist: paintingArtist,
+        };
+        fetch("http://localhost:3000/paintings", {
+            method: "POST",
+            headers: {
+                "COntent-Type": "application/json",
+            },
+            body: JSON.stringify(newPainting),
+        })
+        .then((r) => r.json())
+        .then((painting) => console.log(painting));
+        console.log(newPainting);
+        setPaintingArtist("");
+        setPaintingImage("");
+        setPaintingTitle("");
+    }
+    
+    
     return (
         <div>
-            <h1>This is where you add new paintings!</h1>
+            <form onSubmit={(e) => handleSubmit(e)} className="NewPainting">
+                <label>
+                    Painting Title: 
+                    <input type="text" name="Title" onChange={handleTitleAddition} value={paintingTitle} />
+                </label>
+                <br />
+                <label>
+                    Painting Image: 
+                    <input type="text" name="Image" onChange={handleImageAddition} value={paintingImage} />
+                </label>
+                <br />
+                <label>
+                    Artist Name:
+                    <input type="text" name="Artist" onChange={handleArtistAddition} value={paintingArtist} />
+                </label>
+                <br />
+                <button type="submit">Add Painting</button> 
+            </form>
         </div>
     )
 }
