fca51aa789ce25247acc333f2ccdc7af0ee26d58
samanthamarberger <slatham04@gmail.com>
2023-01-30 17:40:00 -0700

    began styling cards

--- src/components/PaintingCard.js
+++ src/components/PaintingCard.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './PaintingCard.css';
 
 function PaintingCard({ painting }) {
     return(
-        <div>
+        <div classname="card">
             <h2>{painting.title}</h2>
             <img src={painting.imageSrc} alt={painting.title}/>
             <h3>Artist: {painting.artist}</h3>
