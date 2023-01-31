c425f5baed371a147a5b59a3058e0a0fa6fa7fda
samanthamarberger <slatham04@gmail.com>
2023-01-30 18:12:35 -0700

    styled navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
     return (
-        <div className="navbar">
+        <div class="navbar">
             <nav>
-                <Link to="/">Home</Link>
+                <Link to="/" class="home">Home</Link>
             </nav>
             <nav>
-                <Link to="/paintings">Paintings</Link>
+                <Link to="/paintings" class="paintings">Paintings</Link>
             </nav>
             <nav>
-                <Link to="/add-painting">Add a painting</Link>
+                <Link to="/add-painting" class="add">Add a painting</Link>
             </nav>
         </div>
     );
