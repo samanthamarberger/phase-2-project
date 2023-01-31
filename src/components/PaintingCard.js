eb57b13e4278c5349c2721c2a9190fe7a6a972d6
samanthamarberger <slatham04@gmail.com>
2023-01-30 17:59:14 -0700

    styled card

--- src/components/PaintingCard.js
+++ src/components/PaintingCard.js
@@ -3,10 +3,10 @@ import './PaintingCard.css';
 
 function PaintingCard({ painting }) {
     return(
-        <div classname="card">
+        <div class="card">
             <h2>{painting.title}</h2>
             <img src={painting.imageSrc} alt={painting.title}/>
-            <h3>Artist: {painting.artist}</h3>
+            <h3>{painting.artist}</h3>
         </div>
     );
 }
