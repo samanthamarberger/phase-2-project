d956bf7328f80659a7efd20d0b7a0645bc6598b3
samanthamarberger <slatham04@gmail.com>
2023-02-22 10:03:14 -0700

    Done with coding challenge

--- src/components/Paintings.js
+++ src/components/Paintings.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import PaintingCard from "./PaintingCard";
 
 function Paintings({ paintings }) {
-    
+
+    const [search, setSearch] = useState("");
+
+    function handleSearch(e) {
+        // console.log(e.target.value);
+        setSearch(e.target.value);
+    }
+    const newArray = paintings.filter((painting) => painting.artist.toLowerCase().includes(search));
+    console.log(newArray);
+
     return (
         <div>
             <div className="painting-page">
                 <h1>Paintings Page</h1>
             </div>
+            <input onChange={handleSearch}></input>
             <div className="container">
-                {paintings.map((painting) => <PaintingCard key={painting.title} painting={painting} />)}
+                {newArray.map((painting) => <PaintingCard key={painting.title} painting={painting} />)}
             </div>
         </div>
     );
 }
 
 export default Paintings;
+
+//Create a search bar that allows me to search and filter the paintings by artists name 
+
+// Search bar in paintings page
+
+// create a search bar in paintings component
+// OnChange using 
+// pass it the array of paintings and map the paintings depending on the search 
